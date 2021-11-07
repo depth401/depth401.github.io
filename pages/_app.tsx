80123dc4dbf048df3227bdff2b25153178f65694
depth401 <mental_pg@protonmail.com>
2021-11-08 00:24:46 +0900

    Fix sendToGoogleAnalytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,18 @@
 import type { AppProps } from 'next/app';
-import { getCLS, getFID, getLCP } from 'web-vitals';
+import { getCLS, getFID, getLCP, getFCP, getTTFB } from 'web-vitals';
 import usePageView from 'hooks/usePageView';
 
 import '../styles/globals.scss';
+import { existsGaId } from 'lib/gtag';
 
 function sendToGoogleAnalytics({ name, delta, value, id }: any) {
+  if (!existsGaId) {
+    return;
+  }
+
   // Assumes the global `gtag()` function exists, see:
   // https://developers.google.com/analytics/devguides/collection/ga4
-  gtag('event', name, {
+  window.gtag('event', name, {
     // Built-in params:
     value: delta, // Use `delta` so the value can be summed.
     // Custom params:
@@ -18,10 +23,14 @@ function sendToGoogleAnalytics({ name, delta, value, id }: any) {
 }
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  /** Core Web Vitals */
-  getCLS(sendToGoogleAnalytics);
-  getFID(sendToGoogleAnalytics);
-  getLCP(sendToGoogleAnalytics);
+  /** Web Vitals */
+  if (typeof document !== 'undefined') {
+    getCLS(sendToGoogleAnalytics);
+    getFID(sendToGoogleAnalytics);
+    getLCP(sendToGoogleAnalytics);
+    getFCP(sendToGoogleAnalytics);
+    getTTFB(sendToGoogleAnalytics);
+  }
 
   usePageView();
 
