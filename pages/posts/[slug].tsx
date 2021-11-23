d9e6c69eb8d8df0df31abebcd10c941991bb5935
depth401 <mental_pg@protonmail.com>
2021-11-23 22:57:41 +0900

    Add SNS share buttons

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,14 +1,37 @@
 import 'prism-themes/themes/prism-vs.css';
 import Head from 'next/head';
+import { useRouter } from 'next/router';
 import React from 'react';
+import {
+  TwitterShareButton,
+  TwitterIcon,
+  FacebookShareButton,
+  FacebookIcon,
+} from 'react-share';
 import Date from 'components/atoms/Date';
 import Tag from 'components/atoms/Tag';
 import Layout from 'components/templates/Layout';
 import { getAllPostSlugs, getPostBySlug, PostContent } from 'lib/posts';
+import settings from 'settings';
 
 type Props = PostContent;
 
 const Post = ({ slug, frontmatter, contentHtml }: Props) => {
+  const Header = (
+    <div className='justify-center'>
+      <h1 className='text-4xl font-bold pb-4'>{frontmatter.title}</h1>
+      <div className='flex flex-wrap flex-row gap-x-1'>
+        {frontmatter.tags.map((tag, index) => (
+          <span key={index} className='h-auto pr-2 pt-2'>
+            <Tag text={tag} />
+          </span>
+        ))}
+      </div>
+      <Date iso8601={frontmatter.updatedAt} />
+    </div>
+  );
+  const router = useRouter();
+
   return (
     <>
       <Head>
@@ -17,21 +40,30 @@ const Post = ({ slug, frontmatter, contentHtml }: Props) => {
       <Layout title={frontmatter.title}>
         <div className='mx-auto max-w-3xl'>
           <div className='p-5 border rounded-lg shadow-sm'>
-            <div className='justify-center'>
-              <h1 className='text-4xl font-bold pb-4'>{frontmatter.title}</h1>
-              <div className='flex flex-wrap flex-row gap-x-1'>
-                {frontmatter.tags.map((tag, index) => (
-                  <span key={index} className='h-auto pr-2 pt-2'>
-                    <Tag text={tag} />
-                  </span>
-                ))}
-              </div>
-              <Date iso8601={frontmatter.updatedAt} />
-            </div>
+            {Header}
             <div
               className='markdown mt-10 mb-10'
               dangerouslySetInnerHTML={{ __html: contentHtml }}
             ></div>
+            <hr />
+            <div className='mt-5'>
+              <div className='font-bold text-xl'>SHARE</div>
+              <div className='mt-3 flex gap-x-2'>
+                <TwitterShareButton
+                  url={settings.siteMetadata.baseUrl + router.asPath}
+                  title={frontmatter.title}
+                  via={settings.siteMetadata.title}
+                >
+                  <TwitterIcon size={30} round={true}></TwitterIcon>
+                </TwitterShareButton>
+                <FacebookShareButton
+                  url={settings.siteMetadata.baseUrl + router.asPath}
+                  title={frontmatter.title}
+                >
+                  <FacebookIcon size={30} round={true}></FacebookIcon>
+                </FacebookShareButton>
+              </div>
+            </div>
           </div>
         </div>
       </Layout>
--- settings.ts
+++ settings.ts
@@ -3,6 +3,7 @@ const settings = {
     title: '啓蒙の坂',
     author: 'depth401',
     googleSiteVerification: 'JTBFlUsvehnt0ZF-Upbg7uE1WEULFbpYsb48XBRZnzY',
+    baseUrl: 'https://depth401.github.io/',
   },
 };
 
