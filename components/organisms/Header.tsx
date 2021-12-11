88990c6269fe0624f8c32e0c6c456d7dacc44d1c
depth401 <mental_pg@protonmail.com>
2021-12-11 22:16:05 +0900

    Refactoring Header.tsx

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import NavItem from 'components/atoms/NavItem';
 import Title from 'components/atoms/Title';
 import * as gtag from 'lib/gtag';
 
@@ -17,29 +18,17 @@ const ClickEvent = () => {
 const Header = ({ title }: Props) => (
   <header className='flex w-full bg-white border-b items-center flex-wrap pt-4 pb-4 pl-5 pr-5 m-auto top-0 shadow-sm'>
     <Title title={title} />
-    <nav className='ml-8 mr-8'>
-      <span>
-        <Link href='/privacy-policy' prefetch={false}>
-          <a
-            className='font-bold font-mono'
-            style={{ fontFamily: "'Kaisei Decol', self" }}
-            onClick={ClickEvent}
-          >
-            プライバシーポリシー
-          </a>
-        </Link>
-      </span>
-      <span className='ml-6'>
-        <Link href='/disclaimer' prefetch={false}>
-          <a
-            className='font-bold font-mono'
-            style={{ fontFamily: "'Kaisei Decol', self" }}
-            onClick={ClickEvent}
-          >
-            免責事項
-          </a>
-        </Link>
-      </span>
+    <nav className='flex ml-8 gap-x-8'>
+      <NavItem
+        title='プライバシーポリシー'
+        href='/privacy-policy'
+        event={{ label: 'header_privacy_policy' }}
+      />
+      <NavItem
+        title='免責事項'
+        href='/disclaimer'
+        event={{ label: 'header_disclaimer' }}
+      />
     </nav>
   </header>
 );
