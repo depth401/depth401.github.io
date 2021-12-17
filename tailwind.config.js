c0c2c11e43ee9131d1d67ef05ac4874fdffbb202
depth401 <mental_pg@protonmail.com>
2021-12-18 00:54:23 +0900

    Update tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
-  },
-  variants: {
-    extend: {},
-    backgroundColor: ['visited'],
   },
   plugins: [],
 };
